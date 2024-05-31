92c7c7d5577f8c4579abd80d48a97c317722f0e3
MuhammadAreebKazmi2 <mk07202@st.habib.edu.pk>
2024-05-31 23:20:46 +0500

    Conditionals and Data types

---
+++ 1. Data Types/data-type-4.kt
@@ -0,0 +1,9 @@
+fun main() {
+  // character stored in single quote
+  val CharValue: Char = 'a'
+
+  // boolean value
+  val Bool: Boolean = false
+
+  println("my character = $CharValue and Boolean value is $Bool")
+}
---
+++ 2. Conditionals/operators.kt
@@ -0,0 +1,23 @@
+fun main() {
+  var x = 5
+  var y = 3
+
+  val result = x + y
+  println("result 1 is $result")
+  println("result 2 is ${x-y}")
+  println("result 2 is ${x*y}")
+  println("result 2 is ${x/y}")
+
+  // modulus operator
+  println("result 2 is ${x%y}")
+
+  /*
+  can do like
+  x++, ++x
+  x--, --x 
+  like in other languages, works like the same thing
+
+  This is a block comment
+  */
+  
+}
